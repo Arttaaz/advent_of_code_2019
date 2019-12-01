150b8596e9cf699ef0bf41f1bc4c6310069440be
Arttaaz <arttaaz@gmail.com>
2019-12-01 13:17:58 +0100

    day_1 part 2

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -6,14 +6,25 @@ fn main() {
     let input = OpenOptions::new().read(true).open("input").unwrap();
     let buf = BufReader::new(input);
 
-    let result = buf.lines().fold(0, |sum, x| {
+    let mut fuel : u64 = buf.lines().fold(0u64, |sum, x| {
         sum + calculate_fuel(x.unwrap().parse().unwrap())
     });
-    println!("result: {}", result);
+    
+    println!("result: {}", fuel);
 }
 
 
 fn calculate_fuel(module: u64) -> u64 {
-    let fuel = module / 3;
-    fuel - 2
+    let mut fuel = module / 3;
+    fuel -= 2;
+    let mut fuel2 = fuel;
+    while fuel2 > 0 {
+        let tmp : i64 = (fuel2 as i64/3) - 2;
+        if tmp <= 0 {
+            break;
+        }
+        fuel += tmp.clone() as u64;
+        fuel2 = tmp as u64;
+    }
+    fuel
 }
